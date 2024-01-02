f6a256ab5b1f9ae1576aba4bf84fcacef61b40a5
Maherch7 <maherchaouch3@gmail.com>
2024-01-02 11:03:06 +0100

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import FooterUser from './Components/FooterUser';
+import NavUser from './Components/NavUser';
 import UserList from './Components/UserList';
 
 function App() {
   return (
     <div>
+      <NavUser/>
        <UserList/>
+       <FooterUser/>
     </div>
   );
 }
--- src/Components/UserList.js
+++ src/Components/UserList.js
@@ -1,20 +1,29 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
+import CardList from "./CardList"
+import Spinner from 'react-bootstrap/Spinner';
 
 
 
 
 const UserList=()=>{
     const [users,setUsers] = useState([])
+    const [loading,setLoading] = useState(true)
     useEffect(()=>{
         axios.get('https://jsonplaceholder.typicode.com/users')
         .then((res)=>setUsers(res.data))
+        .then(()=>setLoading(false))
         .catch((err)=>console.log(err))
     },[])
     return(
-        <div>
-            {
-                users.map((el,i,t)=><h2>{el.name}</h2>)
+        <div className="cards">
+            {   loading === true ?
+            
+            <Spinner animation="border" role="status">
+                <span className="visually-hidden">Loading...</span>
+                </Spinner>
+                :
+                users.map((el,i,t)=><CardList el={el}/>)
             }
         </div>
     )
